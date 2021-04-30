3ddf90075ba6bc58a3d604d10beea3749175270b
santosh-ui <rajusantosh77@gmail.com>
2021-04-30 10:42:39 +0530

    Mini Book Project upto js in jsx

--- app-test/src/index.js
+++ app-test/src/index.js
@@ -7,6 +7,8 @@
 
 import React from "react";
 import ReactDom from "react-dom";
+// CSS
+import "./index.css";
 
 // Stateless functional component
 // always return jsX
@@ -48,35 +50,106 @@ import ReactDom from "react-dom";
 // Close every element
 // formatting
 
-const Greeting = () => {
-  return (
-    <React.Fragment>
-    <section>
-      <article>
-        <div className="">
-        <h1>Hello Friends</h1>
-          <ul>
-            <li>
-              <a href="#">Hello</a>
-            </li>
-          </ul>
-          <img src="" alt=""/>
-          <input type="text" name="" id=""/>
-        </div>
-      </article>
-    </section>    
-    <div></div> 
-    {/* // only return single element 
-        or instead wrap it in <>..</>*/}
-    </React.Fragment>
-  );
-};
+// const Greeting = () => {
+//   return (
+//     <React.Fragment>
+//     <section>
+//       <article>
+//         <div className="">
+//         <h1>Hello Friends</h1>
+//           <ul>
+//             <li>
+//               <a href="#">Hello</a>
+//             </li>
+//           </ul>
+//           <img src="" alt=""/>
+//           <input type="text" name="" id=""/>
+//         </div>
+//       </article>
+//     </section>    
+//     <div></div> 
+//     {/* // only return single element 
+//         or instead wrap it in <>..</>*/}
+//     </React.Fragment>
+//   );
+// };
 
-ReactDom.render(
-  <Greeting/>, document.getElementById("root")
-); 
+// ReactDom.render(
+//   <Greeting/>, document.getElementById("root")
+// ); 
 
 // --------------------------------------------------------------------------------------------------
+
+// Nested Components and React Tools
+// If you want you can place your whole app in a single component
+// It somewhat defeats the purpose of why you want to use the react
+// in the first place. The idea is that you can split-up your app into
+// nice seperate chunks of functionality
+
+// const Greetings = () => {
+//   return (
+//     <section>
+//       <Person/> 
+//       {/* {components nested inside the root component. */}
+//       <Age/>
+//     </section>
+//   );
+// }
+
+// const Person = () => <h1>Hello! I'm Santosh</h1>;
+// const Age = () => {
+//   return (
+//     <p>I'm {Number(prompt("Enter your age: "))} years old.</p>
+//     // {/* You can write javascpit in react inside single flower brackets like 
+//     //   shown above. */}
+//   );
+// }
+
+// ReactDom.render(
+//   <Greetings/>, document.getElementById("root")
+// ); 
+
+// ------------------------------------------------------------------------------------------
+
+// Mini Book Project
+
+const BookList = () => {
+  return (
+    <section className="bookList">
+      <Book/>
+    </section>
+    
+  );
+}
+
+const author = "Paulo Coelho";
+const Book = () => {
+  const title = "The Alchemist";
+  return (
+    <article className="book">
+      <img 
+src="https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg" 
+alt=""/>
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {/* <p>{let x = 6;}</p> */}
+      <p>{6+6}</p>
+    </article>
+  );
+}
+ 
+
+ReactDom.render(
+  <BookList/>, document.getElementById("root")
+);
+
+
+
+
+
+
+
+// ---------------------------------------------------------------------------------------------------
 
 // I dont like to clean the file so I'm commenting the code below for not effecting the upper part
 
